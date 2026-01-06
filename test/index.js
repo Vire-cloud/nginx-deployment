5efcb3b9d8811a2666a7aaf57c5d1c6e0ecae799
Vire-cloud <virender@amnex.com>
2026-01-06 13:57:15 +0530

    Update index.js

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Cloud Run app is version 10 is running 🚀');
+  res.send('Cloud Run app is version 11 is running 🚀');
 });
 
 app.listen(PORT, () => {
